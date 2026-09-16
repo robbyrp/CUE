1afd97dbf341dff1558be77509935bad9f283a53
robbyrp <robertpana2015@gmail.com>
2026-09-16 18:37:15 +0300

    Added multiple constraints for RegisterUserRequestDTO.java.

--- backend/src/main/java/com/cue/demo/dtos/auth/RegisterUserRequestDTO.java
+++ backend/src/main/java/com/cue/demo/dtos/auth/RegisterUserRequestDTO.java
@@ -1,13 +1,15 @@
 package com.cue.demo.dtos.auth;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record RegisterUserRequestDTO (
-    @NotBlank(message = "username cannot be empty") String username,
-    @NotBlank(message = "password cannot be empty") String password,
+    @Size(min = 3)@NotBlank(message = "username cannot be empty") String username,
+    @Size(min = 8) @NotBlank(message = "password cannot be empty") String password,
     @NotBlank(message = "firstName cannot be empty") String firstName,
     @NotBlank(message = "lastName cannot be empty") String lastName,
-    @NotBlank(message = "email cannot be empty") String email,
+    @Email @NotBlank(message = "email cannot be empty") String email,
     @NotBlank(message = "city cannot be empty") String city
 ){
 }
